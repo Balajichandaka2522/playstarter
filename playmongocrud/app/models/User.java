6599691c3b94d74cfa5835bb8d362e59985dc846
Chandaka Balaji <balaji.chandaka@phenom.com>
2024-05-21 16:55:54 +0530

    crud operation

---
+++ playmongocrud/app/models/User.java
@@ -0,0 +1,34 @@
+package models;
+
+import org.bson.types.ObjectId;
+
+public class User {
+    private ObjectId id;
+    private String name;
+    private String email;
+
+    // Getters and setters
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
